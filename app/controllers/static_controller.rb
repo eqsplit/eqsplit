f1f04117a13a3a643f38371cc872515db7a59715
Tony Winn <hi@tonywinn.me>
2016-11-12 15:09:04 +0000

    static placeholders

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,11 @@
+class StaticController < ApplicationController
+  def front
+
+  end
+
+  def dashboard
+  end
+
+  def profile
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/", to: "static#front"
+  get "/dashboard", to: "static#dashboard"
+  get "/profile", to: "static#profile"
 end
